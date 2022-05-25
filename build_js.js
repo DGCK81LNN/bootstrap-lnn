0621b5ec7d426aba4f65a12a68374e47d6d4ea1d
DGCK81LNN <triluolnn@163.com>
2022-05-25 10:19:38 +0800

    Change esbuild target to es2018

--- build_js.js
+++ build_js.js
@@ -4,6 +4,7 @@ esbuild.buildSync({
   entryPoints: ["src/bootstrap-vue-3.mjs"],
   outfile: "dist/bootstrap-vue-3.min.js",
   nodePaths: ["src/_redirects"],
+  target: ["es2018"],
   bundle: true,
   minify: true,
   sourcemap: true,
